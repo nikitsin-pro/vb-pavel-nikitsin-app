47ffd2d2ab093ecbefd4d62a108419bdd826e141
nikitsin-pro <pavel.nikitsin.pro@gmail.com>
2025-07-14 19:17:48 +0200

    chore: add ESLint and Prettier configuration for Angular project
    - Add .eslintrc.js with Angular and Prettier integration
    - Add .prettierrc and .prettierignore for code formatting
    - Update package.json scripts for linting and formatting
    - Add Angular ESLint and Prettier dependencies to devDependencies
    - Update angular.json to include lint builder
    - Add VSCode settings for ESLint and HTML formatting

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  root: true,
+  ignorePatterns: [],
+  env: {
+    browser: true,
+    es2022: true,
+  },
+  overrides: [
+    {
+      files: ['*.ts'],
+      extends: ['eslint:recommended', 'plugin:@angular-eslint/recommended', 'prettier'],
+      rules: {
+        'no-console': 'warn',
+        'no-unused-vars': 'warn',
+        'no-empty': 'warn',
+      },
+    },
+    {
+      files: ['*.spec.ts'],
+      extends: ['eslint:recommended', 'plugin:@angular-eslint/recommended', 'prettier'],
+      env: {
+        jasmine: true,
+      },
+      rules: {
+        'no-undef': 'off',
+      },
+    },
+  ],
+};
